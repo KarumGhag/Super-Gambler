b71795e01f382a9cfa74299706bf401efd4b4288
Karum Ghag <karumghag09@gmail.com>
2024-09-27 16:53:02 +0100

    made the slots change colour with delay

--- SlotMachine/slot.gd
+++ SlotMachine/slot.gd
@@ -3,18 +3,39 @@ extends Sprite2D
 class_name Slot
 
 var timesToSpin : int
-var colours : Dictionary = {
-	"Red" : Color(255, 0, 0),
-	"Green" : Color(0, 255, 0),
-	"Blue" : Color(0, 0, 255),
-}
+var colours : Array[Color] = [Color(255, 0, 0), Color(0, 255, 0), Color(0, 0, 255)]
 
-var currentColour : Color = colours["Red"]
+var currentColour : Color = colours[0]
 var currentColourNum : int = 0
-var nextColour : int = 1
+var nextColour : int = currentColourNum + 1
+
+var spinCoolDown : Timer = Timer.new()
+var timePerSpin : float = 0.25
+var timesSpun : int = 0
 
 func _ready() -> void:
-	modulate = currentColour
+	genSpin()
+	add_child(spinCoolDown)
+	spinCoolDown.autostart = false
+	spinCoolDown.one_shot = false
+	spinCoolDown.connect("timeout", spin)
+	spinCoolDown.start(timePerSpin)
+	
+
+func genSpin() -> void:
+	timesToSpin = randi_range(0, 25)
 
 func spin() -> void:
-	timesToSpin = randi_range(0, len(colours) - 1)
+	currentColour = colours[nextColour]
+	nextColour += 1
+	timesSpun += 1
+	if timesSpun >= timesToSpin:
+		spinCoolDown.stop()
+
+func changeColour(newColour : Color):
+	currentColour = newColour
+
+func _process(_delta) -> void:
+	modulate = currentColour
+	if nextColour == len(colours):
+		nextColour = 0
